4a85fd0d0183201449689183a4d6af9ffb50dcf6
Volkan 2525 <volkanumudum@outlook.com>
2024-08-31 08:58:11 -0400

    login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,29 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+username != ""
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    users.filter(function(userF) {
+        return userF[0]==username && userF[1]==password;
+    });
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+    console.log(req);
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let usr = req.body.username;
+  let pwd = req.body.password;
+  let result = authenticatedUser(usr,pwd);
+  console.log(result)
+  if(result!="")
+    return res.status(200).json({message: "Login successfull"})
+  else
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
